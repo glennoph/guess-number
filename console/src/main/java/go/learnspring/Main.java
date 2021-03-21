2a7f9fea41a91711553cd6e607737c476a284702
glenn opdycke-hansen <glennoph@gmail.com>
2021-03-21 15:39:53 -0500

    convert application to spring boot

---
+++ console/src/main/java/go/learnspring/Main.java
@@ -0,0 +1,15 @@
+package go.learnspring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("guess the number game");
+        SpringApplication.run(Main.class, args);
+    }
+}
